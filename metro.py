1c55c25872f3dbe3c3ad246d1eff4dba56ce1a2d
Daniel-Alencar <danielalencar746@gmail.com>
2024-06-15 12:21:43 -0300

    feat: Interface

--- metro.py
+++ metro.py
@@ -13,56 +13,52 @@ coordenadas = [
         'coordenada': (195, 278)
     },
     {
-        'name': 'E1',
-        'coordenada': (327, 317)
+        'name': 'E3',
+        'coordenada': (329, 312)
     },
     {
-        'name': 'E1',
+        'name': 'E4',
         'coordenada': (433, 336)
     },
     {
-        'name': 'E1',
+        'name': 'E5',
         'coordenada': (619, 397)
     },
     {
-        'name': 'E1',
+        'name': 'E6',
         'coordenada': (645, 425)
     },
     {
-        'name': 'E1',
-        'coordenada': (220, 76)
-    },
-    {
-        'name': 'E1',
-        'coordenada': (329, 196)
-    },
-    {
-        'name': 'E1',
-        'coordenada': (394, 466)
-    },
-    {
-        'name': 'E1',
-        'coordenada': (460, 98)
-    },
-    {
-        'name': 'E1',
-        'coordenada': (461, 177)
-    },
-    {
-        'name': 'E1',
-        'coordenada': (395, 466)
-    },
-    {
-        'name': 'E1',
-        'coordenada': (368, 524)
-    },
-    {
-        'name': 'E1',
+        'name': 'E7',
         'coordenada': (591, 416)
     },
     {
-        'name': 'E1',
+        'name': 'E8',
+        'coordenada': (461, 177)
+    },
+    {
+        'name': 'E9',
+        'coordenada': (329, 196)
+    },
+    {
+        'name': 'E10',
         'coordenada': (157, 306)
+    },
+    {
+        'name': 'E11',
+        'coordenada': (220, 76)
+    },
+    {
+        'name': 'E12',
+        'coordenada': (460, 98)
+    },
+    {
+        'name': 'E13',
+        'coordenada': (395, 466)
+    },
+    {
+        'name': 'E14',
+        'coordenada': (368, 524)
     },
 ]
 
@@ -108,24 +104,73 @@ button_hover_color = (0, 255, 0)
 # Define a cor do texto do botão
 button_text_color = (255, 255, 255)
 
+metro_path_coordenadas = []
+executed = False
+
+
+def clear_selection():
+    global clicked_states, clicked_circles
+    clicked_states = [False] * len(coordenadas)
+    clicked_circles = []
+    global executed
+    executed = False
+
+    # Limpa as coordenadas das linhas
+    metro_path_coordenadas.clear()
+    print("Seleção limpa.")
+
+def get_coordenadas(metro_path):
+    global metro_path_coordenadas
+
+    for i in range(len(metro_path) - 1):
+
+        for coordenada in coordenadas:
+            if coordenada['name'] == metro_path[i]:
+                start_coord = coordenada['coordenada']
+                break
+        for coordenada in coordenadas:
+            if coordenada['name'] == metro_path[i + 1]:
+                end_coord = coordenada['coordenada']
+                break
+        metro_path_coordenadas.append((start_coord, end_coord))
+
+    global executed
+    executed = True
+
+    return metro_path_coordenadas
+
 # Função a ser chamada quando o botão for clicado
+def button_function(clicked_circles):
+    if platform.system() == 'Windows':
+        exe_path = "./algorithm.exe"
+    elif platform.system() == 'Linux':
+        exe_path = "./algorithm"
+    else:
+        print("Sistema operacional não suportado.")
+        # Encerra o programa com um código de erro
+        exit(1) 
 
-def button_function():
-  if platform.system() == 'Windows':
-      exe_path = "algorithm.exe"
-  elif platform.system() == 'Linux':
-      exe_path = "algorithm.out"
-  else:
-      print("Sistema operacional não suportado.")
-      return
+    # Argumentos a serem passados para o executável
+    args = []
+    for circle in clicked_circles:
+        args.append(circle['name'])
+    print(args)
 
-  # try:
-  #     result = subprocess.run([exe_path], capture_output=True, text=True, check=True)
-  #     print("Saída:", result.stdout)
-  #     print("Erro:", result.stderr)
-  # except subprocess.CalledProcessError as e:
-  #     print(f"Erro ao executar o arquivo: {e}")
+    metro_path = []
+    try:
+        result = subprocess.run(
+            [exe_path] + args, 
+            capture_output=True, 
+            text=True, 
+            check=True
+        )
+        metro_path = result.stdout.split(',')
+        print("Saída:", metro_path)
+    except subprocess.CalledProcessError as e:
+        print(f"Erro ao executar o arquivo: {e}")
 
+    # Coordenadas específicas para conectar com linhas
+    get_coordenadas(metro_path)
 
 # Loop principal do jogo
 running = True
@@ -153,16 +198,25 @@ while running:
                         # Imprime os círculos clicados no console
                         print("Círculos clicados:", clicked_circles)
 
-            # Verifica se o clique está dentro do botão
-            button_rect = pygame.Rect(largura - button_width - 10, altura - button_height - 10, button_width, button_height)
-            if button_rect.collidepoint(click_pos):
-                button_function()
+
+            # Verifica se o clique está dentro dos botões
+            if clear_button_rect.collidepoint(click_pos):
+                clear_selection()
+            elif button_rect.collidepoint(click_pos):
+                button_function(clicked_circles)
+
+            
 
     # Pega a posição do mouse na tela
     mouse_pos = pygame.mouse.get_pos()
 
     # Desenha a imagem de fundo
     screen.blit(background, (0, 0))
+
+    # Desenha o caminho
+    if executed:
+        for start_coord, end_coord in metro_path_coordenadas:
+            pygame.draw.line(screen, (255, 0, 0), start_coord, end_coord, 15)
 
     # Desenha a posição do mouse relativa à imagem na tela
     font = pygame.font.Font(None, 36)
@@ -176,8 +230,13 @@ while running:
         pygame.draw.circle(screen, border_color, coord['coordenada'], circle_radius + border_thickness)
         pygame.draw.circle(screen, current_circle_color, coord['coordenada'], circle_radius)
 
-    # Desenha o botão
-    button_rect = pygame.Rect(largura - button_width - 10, altura - button_height - 10, button_width, button_height)
+    # Desenha o botão Executar
+    button_rect = pygame.Rect(
+        largura - button_width - 10, 
+        altura - button_height - 10, 
+        button_width, 
+        button_height
+    )
     if button_rect.collidepoint(mouse_pos):
         pygame.draw.rect(screen, button_hover_color, button_rect)
     else:
@@ -186,6 +245,25 @@ while running:
     button_font = pygame.font.Font(None, 36)
     button_text = button_font.render("Executar", True, button_text_color)
     button_text_rect = button_text.get_rect(center=button_rect.center)
+    screen.blit(button_text, button_text_rect)
+
+    # Desenha o botão Limpar
+    clear_button_rect = pygame.Rect(
+        10,
+        altura - button_height - 10,
+        button_width, 
+        button_height
+    )
+
+    # Desenha o botão Limpar Seleção
+    if clear_button_rect.collidepoint(mouse_pos):
+        pygame.draw.rect(screen, button_hover_color, clear_button_rect)
+    else:
+        pygame.draw.rect(screen, button_color, clear_button_rect)
+
+    button_font = pygame.font.Font(None, 36)
+    button_text = button_font.render("Limpar", True, button_text_color)
+    button_text_rect = button_text.get_rect(center=clear_button_rect.center)
     screen.blit(button_text, button_text_rect)
 
     # Atualiza o display
---
+++ teste.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+
+int main() {
+  printf("E1,E2,E3,E13,E14");
+}
